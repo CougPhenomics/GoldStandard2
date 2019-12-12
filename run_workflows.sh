2c9fc62437a65aefd71af4415c3a82807ef99eb2
dschneiderch <dominik.schneider@wsu.edu>
2019-12-11 16:38:21 -0800

    doc and update vis for new file format

--- run_workflows.sh
+++ run_workflows.sh
@@ -1,18 +1,19 @@
+#! bash
 
 outdir=output/vis
 mkdir -p $outdir
 
 python \
 $CONDA_PREFIX/bin/plantcv-workflow.py \
---dir data/raw_snapshots/vis \
+--dir data/vis \
 --workflow scripts/visworkflow.py \
 --type png \
 --json $outdir/vis.json \
 --outdir $outdir \
 --adaptor filename \
---delimiter "(.{2})-(.+)-(\d{4}\d{2}\d{2}T\d{2}\d{2}\d{2})-(.+)" \
+--delimiter "(.{2})-(.+)-(\d{8}T\d{6})-(.+)-(\d)" \
 --timestampformat "%%Y%%m%%dT%%H%%M%%S" \
---meta plantbarcode,measurementlabel,timestamp,camera \
+--meta plantbarcode,measurementlabel,timestamp,camera,id \
 --cpu 12 \
 --writeimg \
 --create
