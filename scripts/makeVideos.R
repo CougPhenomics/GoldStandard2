8809816e281f070909514df212d191c51f29765a
dschneiderch <dominik.schneider@wsu.edu>
2019-12-20 16:56:13 -0800

    update makeViedos for 2 roi, no treatment. add .here

--- scripts/makeVideos.R
+++ scripts/makeVideos.R
@@ -3,7 +3,7 @@
 # Input: 
 #   datadir = relative path to data-specific output directory
 #   genotype_mamp = relative path to csv containing genotype info
-# Example: Rscript --vanilla scripts/makeVideos.R "output/from_diy_data" "diy_data/genotype_map.csv"
+# Example: Rscript --vanilla scripts/makeVideos.R "output/psII" "data/genotype_map.csv"
 
 # Get command line arguments
 args = commandArgs(trailingOnly = T)
@@ -19,7 +19,7 @@ load_install = function(pkg){
   if(!require(pkg, character.only = T)){
     install.packages(pkg, character.only =T, repos = 'https://cloud.r-project.org')
     require(pkg, character.only = T)
-    }
+  }
 }
 
 # load all packages
@@ -44,9 +44,7 @@ output = read_csv(file.path(root, datadir,'output_psII_level0.csv'),
 
 # filter gmap for available output files
 gmap = inner_join(gmap, output) %>% distinct(sampleid, roi, gtype) %>% 
-  mutate(treatment = case_when(gtype=='WT'~'control',
-                               TRUE ~ gtype),
-         gtype = 'WT')
+  mutate(treatment = 'control')
 
 # setup roi positions for genotype labels
 nrow = 3
@@ -77,55 +75,40 @@ get_dates = function(fns) {
 # create list of tray pairs for gifs
 fluc_ids = unique(gmap %>% filter(treatment != 'control') %>% pull(sampleid))
 cntrl_ids = unique(gmap %>% filter(treatment == 'control') %>% pull(sampleid))
-l =  cross2(fluc_ids, cntrl_ids)
+if(length(fluc_ids)!=0){
+  l = cross2(fluc_ids, cntrl_ids)
+} else {
+  l = as.list(gmap %>% distinct(sampleid) %>% pull)
+}
+# l=l[c(1,2)]
 
 # test values
 # sampleid_c = 'A1'
 # sampleid_t = 'A2'
-# parameter_string = 'FvFm_YII'#'FvFm_YII' #'t300_ALon_YII'
+# parameter_string = 'FvFm-YII'#'FvFm_YII' #'t300_ALon_YII'
 # il = l[[1]]
 
 # define gif making function
 arrange_gif = function(il, parameter_string) {
   uil = unlist(il, rec = F)
-  sampleid_t = uil[1]
-  sampleid_c = uil[2]
-  gtype_treatment_label_t = get_treatment(sampleid_t)
-  print(paste(sampleid_c, sampleid_t, sep = ' x '))
-  print(gtype_treatment_label_t)
-  print(parameter_string)
-  
+  if(length(uil)==1){
+    sampleid_c = uil
+  } else {
+    sampleid_c = uil[2]
+  }
+  # Get first set of images regardless of number of samples in uil:
   # get images
   fns0 = dir(file.path(indir, sampleid_c),
              pattern = parameter_string,
              full.names = T)
-  fns1 = dir(file.path(indir, sampleid_t),
-             pattern = parameter_string,
-             full.names = T)
-  
-  # get dates from filenames
+  #get dates
   dtes0 = get_dates(fns0)
-  dtes1 = get_dates(fns1)
   
-  # filter for common dates
-  commondtes <- intersect(dtes0,dtes1)
-  elements0 = dtes0 %in% commondtes
-  elements1 = dtes1 %in% commondtes
-  dtes0 <- dtes0[elements0]
-  dtes1 <- dtes1[elements1]
-  fns0 <- fns0[elements0]
-  fns1 <- fns1[elements1]
-
   # get genotypes
   g0 = gmap %>% filter(sampleid == sampleid_c) %>% pull(gtype)
-  g1 = gmap %>% filter(sampleid == sampleid_t) %>% pull(gtype)
-  # crossing(dtes0,dtes1) #TODO: filter dates and filenames for common dates
-  
-  stopifnot(all(dtes0 == dtes1))
   
   # read images
   imgs0 = image_read(fns0)
-  imgs1 = image_read(fns1)
   
   # annotate with genotype
   imgs0a = image_annotate(
@@ -182,78 +165,132 @@ arrange_gif = function(il, parameter_string) {
       }
     )
   
-  
-  imgs1a = image_annotate(
-    imgs1,
-    gtype_treatment_label_t,
-    size = 24,
-    font = 'Arial',
-    weight = 700,
-    gravity = "NorthWest",
-    location = geometry_point(30, 20),
-    color = "white"
-  )
-  imgs1a = image_annotate(
-    imgs1a,
-    dtes1,
-    size = 24,
-    font = 'Arial',
-    weight = 700,
-    gravity = "NorthWest",
-    location = geometry_point(300, 20),
-    color = 'white'
-  )
-  
-  coords %>%
-    filter(sampleid == sampleid_t) %>%
-    group_by(xpos, ypos, roi) %>%
-    group_walk(
-      keep = T,
-      .f = function(df, grp) {
-        # print(paste('grp:',grp, collapse=','))
-        # print(str(df))
-        xpos = df$xpos
-        ypos = df$ypos
-        gtype = df$gtype
-        if (toupper(gtype) == 'WT') {
-          gstyle = 'Normal'
-        } else {
-          gstyle = 'Italic'
-        }
-        # #
-        imgs1a <<-   image_annotate(
-          imgs1a,
-          gtype,
-          font = 'Arial',
-          style = gstyle,
-          weight = 700,
-          size = 24,
-          gravity = 'NorthWest',
-          location = geometry_point(xpos, ypos),
-          color = 'white'
-        )
+  # if there is only 1 set of images save to file. otherwise get second set of images and filter for common dates, append together, write to file
+  if(length(uil)==1){
+    
+    # combine timelapses
+    for (i in 1:length(imgs0)) {
+      if (i == 1) {
+        newgif = imgs0a[i]
+        # newgif = image_append(c(newgif,imgstif[i]), stack=T)
+      } else {
+        combined  <- imgs0a[i]
+        # combined = image_append(c(combined,imgstif[i]), stack=T)
+        newgif <- c(newgif, combined)
       }
-    )
-  
-  # combine timelapses
-  for (i in 1:length(imgs0)) {
-    if (i == 1) {
-      newgif = image_append(c(imgs0a[i], imgs1a[i]))
-      # newgif = image_append(c(newgif,imgstif[i]), stack=T)
-    } else {
-      combined  <- image_append(c(imgs0a[i], imgs1a[i]))
-      # combined = image_append(c(combined,imgstif[i]), stack=T)
-      newgif <- c(newgif, combined)
     }
+    # newgif
+    outfn = paste0(parameter_string, '-', sampleid_c ,'.gif')
+    image_write_video(newgif, file.path(outdir, outfn), framerate = 2)
+    # image_write_gif(newgif,file.path(outdir, outfn), delay=0.5)
+    
+  } else {
+    
+    sampleid_t = uil[1]
+    gtype_treatment_label_t = get_treatment(sampleid_t)
+    print(paste(sampleid_c, sampleid_t, sep = ' x '))
+    print(gtype_treatment_label_t)
+    print(parameter_string)
+    
+    fns1 = dir(file.path(indir, sampleid_t),
+               pattern = parameter_string,
+               full.names = T)
+    
+    # get dates from filenames
+    dtes1 = get_dates(fns1)
+    
+    # filter for common dates
+    commondtes <- intersect(dtes0,dtes1)
+    elements0 = dtes0 %in% commondtes
+    elements1 = dtes1 %in% commondtes
+    dtes0 <- dtes0[elements0]
+    dtes1 <- dtes1[elements1]
+    fns0 <- fns0[elements0]
+    fns1 <- fns1[elements1]
+    
+    # get genotypes
+    g1 = gmap %>% filter(sampleid == sampleid_t) %>% pull(gtype)
+    # crossing(dtes0,dtes1) #TODO: filter dates and filenames for common dates
+    
+    stopifnot(all(dtes0 == dtes1))
+    
+    #read images
+    imgs1 = image_read(fns1)
+    
+    # annotate images
+    
+    imgs1a = image_annotate(
+      imgs1,
+      gtype_treatment_label_t,
+      size = 24,
+      font = 'Arial',
+      weight = 700,
+      gravity = "NorthWest",
+      location = geometry_point(30, 20),
+      color = "white"
+    )
+    imgs1a = image_annotate(
+      imgs1a,
+      dtes1,
+      size = 24,
+      font = 'Arial',
+      weight = 700,
+      gravity = "NorthWest",
+      location = geometry_point(300, 20),
+      color = 'white'
+    )
+    
+    coords %>%
+      filter(sampleid == sampleid_t) %>%
+      group_by(xpos, ypos, roi) %>%
+      group_walk(
+        keep = T,
+        .f = function(df, grp) {
+          # print(paste('grp:',grp, collapse=','))
+          # print(str(df))
+          xpos = df$xpos
+          ypos = df$ypos
+          gtype = df$gtype
+          if (toupper(gtype) == 'WT') {
+            gstyle = 'Normal'
+          } else {
+            gstyle = 'Italic'
+          }
+          # #
+          imgs1a <<-   image_annotate(
+            imgs1a,
+            gtype,
+            font = 'Arial',
+            style = gstyle,
+            weight = 700,
+            size = 24,
+            gravity = 'NorthWest',
+            location = geometry_point(xpos, ypos),
+            color = 'white'
+          )
+        }
+      )
+    
+    # combine timelapses
+    for (i in 1:length(imgs0)) {
+      if (i == 1) {
+        newgif = image_append(c(imgs0a[i], imgs1a[i]))
+        # newgif = image_append(c(newgif,imgstif[i]), stack=T)
+      } else {
+        combined  <- image_append(c(imgs0a[i], imgs1a[i]))
+        # combined = image_append(c(combined,imgstif[i]), stack=T)
+        newgif <- c(newgif, combined)
+      }
+    }
+    # newgif
+    treatmentlabel = strsplit(gtype_treatment_label_t,' ')[[1]][2]
+    outfn = paste0(parameter_string, '_', sampleid_c, '_x_', sampleid_t, '_',treatmentlabel,'.gif')
+    image_write_video(newgif, file.path(outdir, outfn), framerate = 2)
+    # image_write_gif(newgif,file.path(outdir, outfn), delay=0.5)
   }
-  # newgif
-  treatmentlabel = strsplit(gtype_treatment_label_t,' ')[[1]][2]
-  outfn = paste0(parameter_string, '_', sampleid_c, '_x_', sampleid_t, '_',treatmentlabel,'.gif')
-  image_write_video(newgif, file.path(outdir, outfn), framerate = 2)
-  # image_write_gif(newgif,file.path(outdir, outfn), delay=0.5)
 }
 
-param = 'FvFm_YII'
-for (param in c('FvFm_YII', 't300_ALon_YII', 't300_ALon_NPQ')) {
+param = 'FvFm-YII'
+for (param in c('FvFm-YII', 't300_ALon-YII', 't300_ALon-NPQ')) {
   walk(l, arrange_gif, param)
 }
